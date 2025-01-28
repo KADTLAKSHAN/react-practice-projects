4d0125034ed7a85b06a4929150d02d9901627a7a
KADTLAKSHAN <kadtlakshan@gmail.com>
2025-01-28 10:00:50 +0530

    Project Updated - Added Remove listner for Mousemove Project

--- react-mouse-move-project/src/App.jsx
+++ react-mouse-move-project/src/App.jsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import ParentComponent from "./ParentComponent";
 
 function App() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = (event) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
-    };
-    window.addEventListener("mousemove", handleMouseMove);
-  }, []);
-
   return (
     <div>
-      <h2>Mouse Position</h2>
-      <p>
-        X: {mousePosition.x}, Y: {mousePosition.y}
-      </p>
+      <ParentComponent />
     </div>
   );
 }
