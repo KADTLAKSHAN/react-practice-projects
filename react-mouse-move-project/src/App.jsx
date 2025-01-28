f858333a18f9f99c9fa438c200cf2322a2781f60
KADTLAKSHAN <kadtlakshan@gmail.com>
2025-01-28 09:27:40 +0530

    Added React Mouse Move Project

---
+++ react-mouse-move-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  return (
+    <div>
+      <h2>Mouse Position</h2>
+      <p>
+        X: {mousePosition.x}, Y: {mousePosition.y}
+      </p>
+    </div>
+  );
+}
+
+export default App;
